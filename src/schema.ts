01284059a3cbe7646c0e49de1ec39b1cb8836993
Mykhailo Stadnyk <mikhus@gmail.com>
2018-10-13 02:03:30 +0300

    wip

--- src/schema.ts
+++ src/schema.ts
@@ -16,49 +16,23 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  */
-import {
-    GraphQLObjectType,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLString,
-} from 'graphql';
+import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import { wrapResolvers } from 'graphql-validity/lib';
-import { Resolvers } from './helpers';
-import { userType, nodeField } from './entities';
+import { nodeField } from './entities';
 import { updateUser, login, logout } from './mutations';
+import { user, users } from './queries';
 
 /**
  * Defining Query type for GraphQL schema
  */
 const Query: GraphQLObjectType = new GraphQLObjectType({
     name: 'Query',
-    fields: () => ({
-        users: {
-            description: 'Fetches list of users',
-            type: new GraphQLList(userType),
-            resolve: Resolvers.fetchUsers
-        },
-        user: {
-            description: 'Fetches user data by user id or email',
-            type: userType,
-            args: {
-                id: {
-                    type: GraphQLString,
-                    description: 'User identifier. Optional. ' +
-                        'Either this identifier or email required' ,
-                },
-                email: {
-                    type: GraphQLString,
-                    description: 'User email address. Optional. ' +
-                        'Either this email or identifier required',
-                },
-            },
-            resolve: Resolvers.fetchUserByIdOrEmail,
-        },
+    fields: {
+        users,
+        user,
         node: nodeField
-    }),
+    },
 });
-
 
 /**
  * Defining Mutation type for GraphL schema
